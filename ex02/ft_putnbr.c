a694fc99701d3835752744825fbfdc8085aae28b
giorgiomiserendino <93911490+giorgiomiserendino@users.noreply.github.com>
2021-12-13 19:53:46 +0100

    Add files via upload

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnicolai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 10:29:54 by fnicolai          #+#    #+#             */
+/*   Updated: 2021/12/02 21:40:55 by fnicolai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
